d0c60d75b5a3fe160b26069a63eac5ce94c5b14d
SridharaniSenthilKumar <113413573+SridharaniSenthilKumar@users.noreply.github.com>
2023-06-01 15:37:57 +0530

    Add files via upload

---
+++ DAY_5/src/main/java/com/example/test/App.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class App
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
